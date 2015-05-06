1edf035ac22229ef12f144946c7b0fa2b95198d0
David Trupiano <davetrupiano@gmail.com>
2015-05-05 20:50:09 -0400

    updates

--- web2py/applications/ACPE_Service/controllers/default.py
+++ web2py/applications/ACPE_Service/controllers/default.py
@@ -4,14 +4,17 @@ import urllib2
 def report_credit():
     """ redirect ACPE request """
 
-    try:
-        data = json.loads(request.body.read())
-    except:
-        data = None
+    # try:
+    #     data = json.loads(request.body.read())
+    # except:
+    data = None
 
     if data:
         url = data.get('url')
         auth_token = data.get('auth_token')
+    else:
+        url = None
+        auth_token = None
 
     if url and auth_token=="SUPERDUPERLONGAUTHTOKEN":
 
@@ -24,4 +27,4 @@ def report_credit():
         json_string = json.dumps({'errors':'Missing Data For ACPE Request.'})
 
 
-    return json_string
+    return request.body.read()
