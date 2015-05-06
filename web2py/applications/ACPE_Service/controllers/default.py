80c4fcf33c8513d2b1381b84f256b596e39572b9
David Trupiano <davetrupiano@gmail.com>
2015-05-05 20:45:06 -0400

    updates

--- web2py/applications/ACPE_Service/controllers/default.py
+++ web2py/applications/ACPE_Service/controllers/default.py
@@ -1,12 +1,17 @@
+import json
+import urllib2
+
 def report_credit():
     """ redirect ACPE request """
-    import json
-    import urllib2
 
-    data = json.loads(request.body.read())
+    try:
+        data = json.loads(request.body.read())
+    except:
+        data = None
 
-    url = data.get('url')
-    auth_token = data.get('auth_token')
+    if data:
+        url = data.get('url')
+        auth_token = data.get('auth_token')
 
     if url and auth_token=="SUPERDUPERLONGAUTHTOKEN":
 
